6ed76c068484966a0ded5394aab8913700f49939
Omar <omarkhial74@gmail.com>
2021-10-18 16:39:27 +0200

    player animation

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 var speed = 100
 var velocity = Vector2.ZERO
 
+
 func _physics_process(delta):
+	
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 	elif Input.is_action_pressed("move_left"):
@@ -19,19 +21,31 @@ func _physics_process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	move_and_slide(velocity)
-	if Input.is_action_just_pressed("move_right"):
+	
+	if Input.is_action_pressed("move_right"):
+		$BodyElectricien.playing = true
 		$BodyElectricien.flip_h = false
 		$BodyElectricien.animation = "right"
+		$BodyCommandant.playing = true
 		$BodyCommandant.flip_h = false
 		$BodyCommandant.animation = "right"
-	if Input.is_action_just_pressed("move_down"):
+	elif Input.is_action_pressed("move_down"):
+		$BodyCommandant.playing = true
+		$BodyElectricien.playing = true
 		$BodyElectricien.animation = "down"
 		$BodyCommandant.animation = "down"
-	if Input.is_action_just_pressed("move_up"):
+	elif Input.is_action_pressed("move_up"):
+		$BodyCommandant.playing = true
+		$BodyElectricien.playing = true
 		$BodyElectricien.animation = "up"
 		$BodyCommandant.animation = "up"
-	if Input.is_action_just_pressed("move_left"):
+	elif Input.is_action_pressed("move_left"):
+		$BodyCommandant.playing = true
+		$BodyElectricien.playing = true
 		$BodyElectricien.animation = "right"
 		$BodyElectricien.flip_h = true
 		$BodyCommandant.animation = "right"
 		$BodyCommandant.flip_h = true
+	else:
+		$BodyCommandant.playing = false
+		$BodyElectricien.playing = false
