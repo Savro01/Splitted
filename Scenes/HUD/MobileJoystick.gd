39ea63894d56bcfcf8d9db98d80d04dac49d6f96
Savro01 <59883445+Savro01@users.noreply.github.com>
2021-12-13 10:17:56 +0100

    Bug regler
    voila

--- Scenes/HUD/MobileJoystick.gd
+++ Scenes/HUD/MobileJoystick.gd
@@ -2,10 +2,11 @@ extends CanvasLayer
 
 
 signal use_move_vector
-signal use_A_btn
 
 var move_vector = Vector2(0,0)
 var joystick_active = false
+var a = InputEventAction.new()
+var b = InputEventAction.new()
 
 func _input(event):
 	if event is InputEventScreenTouch or event is InputEventScreenDrag:
@@ -15,37 +16,36 @@ func _input(event):
 			joystick_active = true
 			$Innercircle.position = event.position
 			$Innercircle.visible = true
-			
-		if $A_Button.is_pressed():
-			var a = InputEventAction.new()
-			a.action = "object_interact"
-			a.pressed = true
-			Input.parse_input_event(a)
-
-			
-		if $B_Button.is_pressed():
-			var b = InputEventAction.new()
-			b.action = "ui_cancel"
-			b.pressed = true
-			Input.parse_input_event(b)
-
-			
+		if(get_parent().get_parent().get_parent().currentArea != null):
+			if $A_Button.is_pressed():
+					set_a_pressed()
+			if $B_Button.is_pressed():
+					set_b_pressed()
 	if event is InputEventScreenTouch:
 		if event.pressed == false:
 			joystick_active = false
 			$Innercircle.visible = false
-			
-
-				
 
 func _physics_process(delta):
 	if joystick_active:
 		emit_signal("use_move_vector", move_vector)
 	else:
 		emit_signal("use_move_vector", Vector2())
-	
-
 
 func calculate_move_vector(event_position):
 	var texture_center = $TouchScreenButton.position + Vector2(80,80)
 	return (event_position - texture_center).normalized()
+
+func set_a_pressed():
+	a.action = "object_interact"
+	a.pressed = true
+	b.pressed = false
+	Input.parse_input_event(a)
+	Input.parse_input_event(b)
+
+func set_b_pressed():
+	b.action = "ui_cancel"
+	b.pressed = true
+	a.pressed = false
+	Input.parse_input_event(b)	
+	Input.parse_input_event(a)
