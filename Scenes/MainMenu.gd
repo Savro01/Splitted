ddc975daa2d6e940f0d4c6f262e8adfbb573b41b
Omar <omarkhial74@gmail.com>
2021-12-14 21:46:11 +0100

    fullscreen options

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -7,30 +7,27 @@ extends Control
 
 #signal set_connect_type
 
+func updateFullScreenBtn():
+	if(!OS.window_fullscreen):
+		$OptionsContainer/Fullscreen.text = "Windowed"
+	else:
+		$OptionsContainer/Fullscreen.text = "Fullscreen"
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#get_tree().connect("connected_to_server", self, "connected")
-	pass
-
+		updateFullScreenBtn()
+		
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if $RejoindreContainer.visible and Input.is_action_pressed("ui_cancel"):
-		$AnimationMenu.play_backwards("AnimationMenu")
-		var t = Timer.new()
-		t.set_wait_time(1)
-		self.add_child(t)
-		t.start()
-		yield(t, "timeout")
-		$RejoindreContainer.hide()
+		backToMenu($RejoindreContainer)
 		
 	if $Attente.visible and Input.is_action_pressed("ui_cancel"):
-		$AnimationMenu.play_backwards("AnimationMenu")
-		var t = Timer.new()
-		t.set_wait_time(1)
-		self.add_child(t)
-		t.start()
-		yield(t, "timeout")
-		$Attente.hide()
+		backToMenu($Attente)
+		
+	if $OptionsContainer.visible and Input.is_action_pressed("ui_cancel"):
+		backToMenu($OptionsContainer)
 
 func _on_Rejoindre_pressed():
 	$RejoindreContainer.show()
@@ -45,6 +42,18 @@ func _on_Heberger_pressed():
 
 func _on_Quitter_pressed():
 	get_tree().quit()
+	
+func _on_Options_pressed():
+	$OptionsContainer.show()
+	$AnimationMenu.play("AnimationMenu")
+	
+
+func _on_Fullscreen_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
+	updateFullScreenBtn()
+	if(!OS.window_fullscreen):
+		OS.set_window_size(Vector2(820,480))
+	
 
 func _on_Connexion_pressed():
 	if($RejoindreContainer/IpField != null):
@@ -72,3 +81,19 @@ func start_game(as_server):
 	yield(Network, "game_ready")
 	print("Game Ready")
 	get_tree().change_scene("res://Scenes/Map/Game.tscn")
+
+
+
+func backToMenu(obj):
+	if obj.visible:
+		$AnimationMenu.play_backwards("AnimationMenu")
+		var t = Timer.new()
+		t.set_wait_time(1)
+		self.add_child(t)
+		t.start()
+		yield(t, "timeout")
+		obj.hide()
+
+
+func _on_Retour_pressed():
+	backToMenu($OptionsContainer)
